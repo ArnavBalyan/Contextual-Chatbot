0b9f1e7bd8607f12da9afd748e2457e6990f6476
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2020-06-05 18:52:41 +0530

    Add API

--- flask.py
+++ flask.py
@@ -9,3 +9,7 @@ class Display(Resource):
 	def get(self, string1):
 		headers = {'Content-Type': 'text/html'}
 		return make_response(render_template("result.html",result= "{}".format(string1)))
+api.add_resource(Display, '/search/v1/<string:string1>')
+
+if __name__ == '__main__':
+	app.run()
