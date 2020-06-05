640ffeeb35e84ed359698955aae5401b0743deeb
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2020-06-05 18:52:22 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,1 +1,11 @@
+from flask import Flask, Response, request
+from flask import render_template, make_response
+from flask_restful import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
+
+class Display(Resource):
+	def get(self, string1):
+		headers = {'Content-Type': 'text/html'}
+		return make_response(render_template("result.html",result= "{}".format(string1)))
